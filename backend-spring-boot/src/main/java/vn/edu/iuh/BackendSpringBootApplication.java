2f7cfb1b9b8d632a105208c45add808ff8b92d86
Nguyễn Minh Quân <20098151.quan@student.iuh.edu.vn>
2023-09-04 01:49:44 +0700

    initialization

---
+++ backend-spring-boot/src/main/java/vn/edu/iuh/BackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringBootApplication.class, args);
+    }
+
+}
